106dbea9b16e708039acac8a386b1c97e910c374
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-03-22 18:29:52 +0200

    ex02 Initial version

---
+++ module-00/ex02/Account.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vshchuki <vshchuki@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 17:52:02 by vshchuki          #+#    #+#             */
+/*   Updated: 2024/03/22 18:00:12 by vshchuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+static int	Account::getNbAccounts(void)
+{
+
+}
+
+static int	Account::getTotalAmount(void)
+{
+
+}
+
+static int	Account::getNbDeposits(void)
+{
+
+}
+
+static int	Account::getNbWithdrawals(void)
+{
+
+}
+
+static void	Account::displayAccountsInfos(void)
+{
+
+}
+
+Account::Account( int initial_deposit );
+Account::~Account( void );
+
+void	Account::makeDeposit( int deposit )
+{
+
+};
+
+bool	Account::makeWithdrawal( int withdrawal );
+int		Account::checkAmount( void ) const;
+void	Account::displayStatus( void ) const;
