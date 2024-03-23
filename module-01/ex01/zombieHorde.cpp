5100ec163c8dc8fb5275be6e10caa13a397ae8fd
MCP <91376232+madfcat@users.noreply.github.com>
2024-03-23 22:16:50 +0200

    Remove casting void of name arg

--- module-01/ex01/zombieHorde.cpp
+++ module-01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: vshchuki <vshchuki@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 14:43:17 by vshchuki          #+#    #+#             */
-/*   Updated: 2024/03/23 21:06:33 by vshchuki         ###   ########.fr       */
+/*   Updated: 2024/03/23 22:14:39 by vshchuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 Zombie*	zombieHorde( int N, std::string name )
 {
-	// (void)name;
 	Zombie* horde = new Zombie[N];
 
 	for (int i = 0; i < N; ++i) {
 		horde[i] = Zombie(name);
 	}
-
 	return (horde);
 }
