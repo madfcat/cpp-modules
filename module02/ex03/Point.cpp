3440833550c42180cac0284e66d95f92dee295ea
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-04-25 15:29:13 +0300

    Update x and y to const Fixed in Point in  module02 ex03

--- module02/ex03/Point.cpp
+++ module02/ex03/Point.cpp
@@ -6,37 +6,26 @@
 /*   By: vshchuki <vshchuki@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 22:49:40 by vshchuki          #+#    #+#             */
-/*   Updated: 2024/03/28 00:46:43 by vshchuki         ###   ########.fr       */
+/*   Updated: 2024/04/25 15:27:21 by vshchuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Point::Point()
+Point::Point(): x(Fixed(0)), y(Fixed(0))
 {
-	(this->x) = Fixed(0);
-	(this->y) = Fixed(0);
 }
 
-Point::Point(const float x, const float y)
+Point::Point(const float x, const float y): x(Fixed(x)), y(Fixed(y))
 {
-	this->x = Fixed(x);
-	this->y = Fixed(y);
 }
 
-Point::Point(const Point &point)
+Point::Point(const Point &point): x(Fixed(point.getX())), y(Fixed(point.getY()))
 {
-	this->setX(point.getX());
-	this->setY(point.getY());
 }
 
 Point& Point::operator=(const Point& point)
 {
-	if (this != &point)
-	{
-		this->setX(point.getX());
-		this->setX(point.getX());
-	}
 	return (*this);
 }
 
@@ -54,12 +43,12 @@ Fixed Point::getY(void) const
 	return (this->y);
 }
 
-void Point::setX(const Fixed x)
-{
-	this->x = x;
-}
+// void Point::setX(const Fixed x)
+// {
+// 	this->x = x;
+// }
 
-void Point::setY(const Fixed y)
-{
-	this->y = y;
-}
+// void Point::setY(const Fixed y)
+// {
+// 	this->y = y;
+// }
